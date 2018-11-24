3d8240aee2860717c44ee823c4a0ce8a5e16a181
Paws <lyric_wolf@outlook.com>
2018-11-24 22:10:42 +0000

    Beta changes

--- utils/repo.py
+++ utils/repo.py
@@ -4,23 +4,28 @@ version = "v1.3.0"
 invite = "https://discord.gg/s4bSSCG"
 owners = default.get("config.json").owners
 contributors = default.get("config.json").contributors
+beta = default.get("config.json").beta
 
 
 def is_owner(ctx):
     return ctx.author.id in owners
 
 
+def is_beta(ctx):
+    return ctx.author.id in beta
+
+
 def is_contributor(ctx):
     return ctx.author.id in contributors
 
 
-def has_userID(ctx, userID):
-    return ctx.author.id in userID
+def has_userid(ctx, userid):
+    return ctx.author.id in userid
 
 
-def has_guildID(ctx, guildID):
-    return ctx.guild.id in guildID
+def has_guildid(ctx, guildid):
+    return ctx.guild.id in guildid
 
 
-def has_channelID(ctx, channelID):
-    return ctx.channel.id in channelID
+def has_channelid(ctx, channelid):
+    return ctx.channel.id in channelid
