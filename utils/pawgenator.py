f4f3a0c937d5f9c086375ac930ef8f9167091225
Paws <lyric_wolf@outlook.com>
2019-01-14 18:54:03 +0000

    clear reactions rather than delet

--- utils/pawgenator.py
+++ utils/pawgenator.py
@@ -124,7 +124,7 @@ class SimplePaginator:
 
     async def stop_controller(self, message):
         try:
-            await message.delete()
+            await message.clear_reactions()
         except discord.HTTPException:
             pass
 
